73023e5ddceec6b1d510c363cc337eb4a7dd0ffd
Nikhil-pan <112177478+Nikhil-pan@users.noreply.github.com>
2022-10-12 20:21:53 +0530

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,28 +1,19 @@
-// C++ program for implementation of Heap Sort
+
 
 #include <iostream>
 using namespace std;
 
-// To heapify a subtree rooted with node i
-// which is an index in arr[].
-// n is size of heap
 void heapify(int arr[], int N, int i)
 {
 
-	// Initialize largest as root
 	int largest = i;
-
-	// left = 2*i + 1
 	int l = 2 * i + 1;
-
-	// right = 2*i + 2
 	int r = 2 * i + 2;
 	if (l < N && arr[l] > arr[largest])
 		largest = l;
 	if (r < N && arr[r] > arr[largest])
 		largest = r;
 
-	// If largest is not root
 	if (largest != i) {
 		swap(arr[i], arr[largest]);
 
@@ -52,13 +43,11 @@ void printArray(int arr[], int N)
 	cout << "\n";
 }
 
-// Driver's code
 int main()
 {
 	int arr[] = { 12, 11, 13, 5, 6, 7 };
 	int N = sizeof(arr) / sizeof(arr[0]);
 
-	// Function call
 	heapSort(arr, N);
 
 	cout << "Sorted array is \n";
